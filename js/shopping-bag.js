1964e9d7090c48edb75ff68779d6f5b527c3eb5d
전수안 <dgsw.ahn@dgsw.hs.kr>
2025-07-12 23:58:44 +0900

    feat: 장바구니와 연결

--- js/shopping-bag.js
+++ js/shopping-bag.js
@@ -116,6 +116,270 @@ selectedItems.forEach(selected => {
         goodsEx2 = '음식물과 일반 쓰레기를 줄여요!';
         goodsPrice = 12000;
     }
+    else if (selected == 'sambeTowel') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202505/951e8f0bb2cf8d15d3227377475b4c6d.jpg";
+        goodsName = '친환경 삼베 샤워타올';
+        goodsEx1 = '생분해성 삼베 샤워타올';
+        goodsEx2 = '플라스틱 섬유 사용을 줄여요!';
+        goodsPrice = 11900;
+    } else if (selected == 'bodywashbar') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202309/2b2c7e2235cfb68f71f9b03988281a8a.jpg";
+        goodsName = '약산성 바디워시바';
+        goodsEx1 = '약산성 바디워시바';
+        goodsEx2 = '우리 몸도 지키고 플라스틱 통 사용도 줄여요!';
+        goodsPrice = 4300;
+    } else if (selected == 'toothbrush') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202505/6aa87139f46cc71f57ec2e131bfebf98.png";
+        goodsName = '목재 칫솔';
+        goodsEx1 = '버려지는 나무로 만든 CXP 목재 칫솔';
+        goodsEx2 = '베어지지 않은 나무들을 지켜요!';
+        goodsPrice = 2000;
+    } else if (selected == 'soupShelf') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202408/c138e25e1258be5bf4228ced172d5710.jpg";
+        goodsName = '심플 사각 비누받침';
+        goodsEx1 = '친환경 CXP 목재 비누받침';
+        goodsEx2 = '버려지는 플라스틱 양을 줄여요!';
+        goodsPrice = 4000;
+    } else if (selected == 'toothbrushCap') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202306/9071a03ef16c2b85c2f7fd82259b2a00.jpg";
+        goodsName = '실리톤 칫솔캡';
+        goodsEx1 = '오래 사용 가능한 실리콘 칫솔캡';
+        goodsEx2 = '플라스틱 사용을 줄여요!';
+        goodsPrice = 20000;
+    } else if (selected == 'makeup') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202307/dac47cc81b779d6009cf31784b791453.jpg";
+        goodsName = '순면 화장솜';
+        goodsEx1 = '유기농 순면 화장솜';
+        goodsEx2 = '플라스틱 섬유 사용을 줄여요!';
+        goodsPrice = 3000;
+    } else if (selected == 'tougue') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202402/ba2e6193b228ee1a35e63a234d67df39.jpg";
+        goodsName = '목재 혀클리너';
+        goodsEx1 = 'CXP 목재 혀클리너';
+        goodsEx2 = '플라스틱 사용을 줄여요!';
+        goodsPrice = 2000;
+    } else if (selected == 'squeeze') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202203/9ae9eb45b4675c9b21cf6f3bdd80f371.jpg";
+        goodsName = '스테인리스 치약 짜개';
+        goodsEx1 = '스테인리스로 오래 사용 가능한 치약 짜개';
+        goodsEx2 = '플라스틱 사용을 줄여요!';
+        goodsPrice = 2000;
+    } else if (selected == 'sugarCane') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202011/5024dcdeb85c03cfa7467e0897d9781d.jpg";
+        goodsName = '사탕수수 칫솔';
+        goodsEx1 = '사탕수수로 만든 친환경 칫솔';
+        goodsEx2 = '플라스틱 사용을 줄여요!';
+        goodsPrice = 12000;
+    } else if (selected == 'slipper') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202411/5e32f4830348501bf91b7b16d0212346.jpg";
+        goodsName = '재생펠트 슬리퍼';
+        goodsEx1 = '버려지는 호텔 침구 업사이클링 슬리퍼';
+        goodsEx2 = '재활용에 동참해요!';
+        goodsPrice = 16000;
+    } else if (selected == 'soup') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202503/ff7aac2e82b93b6d3058a4fb1bb5afd7.jpg";
+        goodsName = '몽돌 비누';
+        goodsEx1 = '비지, 커피박으로 만든 만든 몽돌 비누';
+        goodsEx2 = '음식물과 일반 쓰레기를 줄여요!';
+        goodsPrice = 12000;
+    }else if (selected == 'glassCup') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202505/079bd5243981c67a88e16a200f690da8.jpg";
+        goodsName = '천연 글라스컵';
+        goodsEx1 = '천연 유리 특유의 질감이 매력인 글라스컵';
+        goodsEx2 = '플라스틱 사용을 줄여요!';
+        goodsPrice = 12900;
+    } else if (selected == 'kitchenTowel') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202409/00e1cdb19cb5ed49212a9697ee9b1b5e.jpg";
+        goodsName = '대나무 키친타올';
+        goodsEx1 = '무표백 대나무 펄프로 만든 키친타올';
+        goodsEx2 = '베어지지 않은 나무를 지켜요!';
+        goodsPrice = 20000;
+    } else if (selected == 'tumblerBag') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202407/2b1239dce2937f49ad6f4bb49dbcbe10.jpg";
+        goodsName = '텀블러 보온보냉백';
+        goodsEx1 = '폐플라스틱을 재활용해 만든 텀블러 가방';
+        goodsEx2 = '버려지는 플라스틱 양을 줄여요!';
+        goodsPrice = 26000;
+    } else if (selected == 'tumbler') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202310/ca22fca894b2267f1abe28d412cf9e2d.jpg";
+        goodsName = '사탕수수 텀블러';
+        goodsEx1 = '사탕수수로 만든 친환경 텀블러';
+        goodsEx2 = '버려져도 금방 썩어 지구를 지켜요!';
+        goodsPrice = 25000;
+    } else if (selected == 'zipperBag') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202411/7a2e9a4d16c8b86d5fb05f8d2a2e7dad.jpg";
+        goodsName = '실리콘 지퍼백';
+        goodsEx1 = '실리콘으로 만든 다회용 지퍼백';
+        goodsEx2 = '일회용품 사용을 줄여요!';
+        goodsPrice = 1000;
+    } else if (selected == 'cuttingBoard') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202309/60b1b79d2e5febb90cc47bfaeb0b1a6e.png";
+        goodsName = '목재 도마';
+        goodsEx1 = '친환경 CXP 목재로 만든 도마';
+        goodsEx2 = '베어지지 않은 나무를 지켜요!';
+        goodsPrice = 20000;
+    } else if (selected == 'spoonSet') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202209/d763ec57a54ba2c9f60501676cdcd5d3.jpg";
+        goodsName = '다회용 스푼, 포크 세트';
+        goodsEx1 = 'CXP 목재로 만든 다회용 스푼,포크 세트';
+        goodsEx2 = '베어지지 않은 나무를 지켜요!';
+        goodsPrice = 5000;
+    } else if (selected == 'bottleBrush') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202209/62eeb0f91f901a905dfacd87e4c2b0ad.jpg";
+        goodsName = '코코넛 야자 병 세척솔';
+        goodsEx1 = '코코넛 껍질과 느티나무로 만든 친환경 병 세척솔';
+        goodsEx2 = '플라스틱 사용을 줄여요!';
+        goodsPrice = 12000;
+    } else if (selected == 'soup') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202309/510b681e75a05347fdfad5c0339363c9.png";
+        goodsName = '스탠드 주걱';
+        goodsEx1 = 'CXP 목재로 만든 친환경 주걱';
+        goodsEx2 = '편리한데 나무를 지켜요!';
+        goodsPrice = 5500;
+    }else if (selected == 'backpack') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202407/6ba28fa31d9fdfa3ac016b5d362b7be0.jpg";
+        goodsName = '키즈 백팩';
+        goodsEx1 = '폐플라스틱 업사이클링 키즈 백팩';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 59000;
+    } else if (selected == 'cap') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202203/c5c12f9c21493f132a632392a3815a60.jpg";
+        goodsName = '캡모자';
+        goodsEx1 = '폐플라스틱 업사이클링 캡모자';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 43000;
+    } else if (selected == 'hoodie') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202412/00ac0ccde36bc355e0bd29a4c6f96960.jpg";
+        goodsName = '후드티';
+        goodsEx1 = '폐플라스틱 업사이클링 후드티';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 59000;
+    } else if (selected == 'veganCard') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202304/4c6159d45dcb1e256141c8e83650c5df.jpg";
+        goodsName = '비건 카드 지갑';
+        goodsEx1 = '옥수수가죽 카드 지갑';
+        goodsEx2 = '버려져도 금방 썩어요!';
+        goodsPrice = 66000;
+    } else if (selected == 'passport') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202503/4b2bbb19000943e65c5eca9c699b5bbc.jpg";
+        goodsName = '가죽 여권 케이스';
+        goodsEx1 = '가죽 스트랩을 모아 만든 여권 케이스';
+        goodsEx2 = '버려지는 가죽을 줄여요!';
+        goodsPrice = 15000;
+    } else if (selected == 'walkingBag') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202407/210fc9a4e277bf9adbae3da7b317249a.jpg";
+        goodsName = '워킹백';
+        goodsEx1 = '폐플라스틱 업사이크링 워킹백';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 50000;
+    } else if (selected == 'cushion') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202412/da93694f9de654f67165b40227d95965.jpg";
+        goodsName = '쿠션';
+        goodsEx1 = '폐플라스틱 업사이클링 쿠션';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 8000;
+    } else if (selected == 'hairBand') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202501/05a773ec0934146dd43d322cf9fd4cfb.png";
+        goodsName = '헤어밴드';
+        goodsEx1 = '폐플라스틱 업사이클링 헤어밴드';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 12000;
+    } else if (selected == 'homeWear') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202412/25398d9c6fd903f53d429f60a94f800f.jpg";
+        goodsName = '잠옷 세트';
+        goodsEx1 = '폐플라스틱 업사이클링 잠옷 세트';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 52000;
+    } else if (selected == 'tablet') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202412/8ac6a6c2cc603f1ede45bfffd6dfd3ff.jpg";
+        goodsName = '태블릿 파우치';
+        goodsEx1 = '폐플라스틱 업사이클링 태블릿 파우치';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 38000;
+    } else if (selected == 'umbrella') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202309/495c57244175415a595ec6e403ae4759.jpg";
+        goodsName = '장우산';
+        goodsEx1 = '폐플라스틱 업사이클링 장우산';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 22000;
+    }
+    else if (selected == 'note') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202310/b8f3de03a2c7790db0cb566ab72bb2b0.jpg";
+        goodsName = '코끼리똥 노트';
+        goodsEx1 = '코끼리똥으로 만든 종이를 모은 노트';
+        goodsEx2 = '나무를 지켜요!';
+        goodsPrice = 4500;
+    } else if (selected == 'pencilCut') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202309/0f2b49bbee75e89c60ac39514a028175.jpg";
+        goodsName = '폐나무 연필깎이';
+        goodsEx1 = '버려지는 대나무로 만든 연필깎이';
+        goodsEx2 = '나무를 지켜요!';
+        goodsPrice = 3300;
+    } else if (selected == 'pencil') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202403/2c99731eca5a6384f09a85e3d7250837.jpg";
+        goodsName = '신문지 연필 세트';
+        goodsEx1 = '버려진 신문지로 만든 연필';
+        goodsEx2 = '나무를 지켜요!';
+        goodsPrice = 3000;
+    } else if (selected == 'pencilCap') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202406/ff6d9972d0acb55636dbca9382ba1e59.jpg";
+        goodsName = '재생 가죽 연필캡';
+        goodsEx1 = '버려지는 가죽으로 만든 연필캡';
+        goodsEx2 = '버려지는 가죽 양을 줄여요!';
+        goodsPrice = 3000;
+    } else if (selected == 'pencilCase') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202409/a2d01df4a83139aaeb2e116e4e280333.jpg";
+        goodsName = '재생 가죽 필통';
+        goodsEx1 = '가죽 조각을 모아 만든 필통';
+        goodsEx2 = '버려지는 가죽 양을 줄여요!';
+        goodsPrice = 7000;
+    } else if (selected == 'eraser') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202306/3448222ffff4df94c25a440d21783d31.jpg";
+        goodsName = '고무 지우개';
+        goodsEx1 = '천연 고무 지우개';
+        goodsEx2 = '플라스틱 사용을 줄여요!';
+        goodsPrice = 3000;
+    } else if (selected == 'coffeePencil') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202402/cfd25d1c9259692829c68a402c19bf29.jpg";
+        goodsName = '커피 연필';
+        goodsEx1 = '커피박으로 만든 연필';
+        goodsEx2 = '일반 쓰레기를 줄여요!';
+        goodsPrice = 3000;
+    } else if (selected == 'ballPen') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202505/84a2d64356d568a6e53e4e568d44c65c.jpg";
+        goodsName = '목재 볼펜';
+        goodsEx1 = 'CXP 목재 볼펜';
+        goodsEx2 = '플라스틱 사용을 줄이고 나무를 지켜요!';
+        goodsPrice = 1500;
+    } else if (selected == 'memo') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202011/58cb90c00b4cd0b33fcab4f4d8bbfb38.jpg";
+        goodsName = '재생 메모지 (100매)';
+        goodsEx1 = '재생펄프로 만든 친환경 포스트잇';
+        goodsEx2 = '나무를 지켜요!';
+        goodsPrice = 2000;
+    } else if (selected == 'set') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202408/5b3042a808bb54ad534a586aac7e1ca4.jpg";
+        goodsName = '친환경 문구 세트';
+        goodsEx1 = '친환경 문구 세트를 선물해요!';
+        goodsEx2 = '지구도 지키고 친구도 기쁘게!';
+        goodsPrice = 10000;
+    } else if (selected == 'book') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202406/66ec823dfc73b4d8dfac628ded4644a4.jpg";
+        goodsName = '가죽 코너 북 커버';
+        goodsEx1 = '버려진 가죽으로 만든 코너 북 커버';
+        goodsEx2 = '버려지는 가죽 양을 줄여요!';
+        goodsPrice = 3000;
+    } else if (selected == 'clip') {
+        imgSrc = "https://onlyeco.co.kr/web/product/big/202112/e857f17271c5b41d1fbbd794048e6158.jpg";
+        goodsName = '클립';
+        goodsEx1 = '폐플라스틱 업사이클링 클립';
+        goodsEx2 = '버려지는 플라스틱을 줄여요!';
+        goodsPrice = 8000;
+    }
+    
+    
+    
+    
     // 구매 버튼이 눌린 상품을 나열하는 공간의 가져옴
     const itemsContainer = document.getElementById('itemsContainer');
     // 상품 내용을 담을 div 생성
