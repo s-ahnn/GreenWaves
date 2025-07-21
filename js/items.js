31dc83e7e62e381a55aff749e275a2def020298f
전수안 <dgsw.ahn@dgsw.hs.kr>
2025-07-21 11:15:12 +0900

    fix: 장바구니에 상품이 담기지 않는 오류 해결

--- js/items.js
+++ js/items.js
@@ -28,14 +28,14 @@ buying.forEach((button, index) => {
 
         // 상품 목록 저장
         //로컬 저장소에 있는 값을 불러와서 (문자열 상태) 객체로 변환 (리스트)
-        let selectedItems = JSON.parse(localStorage.getItem('selectedItems'));
+        let selectedItems = JSON.parse(localStorage.getItem('selectedItems'))||[];
         // 새로 받아온 값을 리스트에 추가
         selectedItems.push(productValue);
         // 리스트를 문자열로 변환해서 로컬저장소에 삽입
         localStorage.setItem('selectedItems', JSON.stringify(selectedItems));
 
         // 수량 저장 (객체로)
-        let selectedAmount = JSON.parse(localStorage.getItem('selectedAmount'));
+        let selectedAmount = JSON.parse(localStorage.getItem('selectedAmount'))||{};
         selectedAmount[productValue] = parseInt(productAmount);
         localStorage.setItem('selectedAmount', JSON.stringify(selectedAmount));
     });
